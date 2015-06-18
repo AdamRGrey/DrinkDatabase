ae2b41b447781bf2c1a2c1af741ddbc02cdd1f25
AdamRGrey <adam.r.grey@gmail.com>
2015-06-18 18:11:13 -0400

    fixed some dumb mistakes

--- DrinkDatabase/Scripts/DrinkEditAjax.js
+++ DrinkDatabase/Scripts/DrinkEditAjax.js
@@ -1,4 +1,25 @@
-﻿$(document).ready(function () {
+﻿function AjaxSave(event) {
+    event.preventDefault();
+   
+    var formData = $('#editDrinkForm');
+    formData.__RequestVerificationToken =  $('#editDrinkForm input[name="__RequestVerificationToken"]').val(); //there's one in the logout header. Make sure we have the *right* antiforgery token.
+
+    console.log("we'll be sending this formData.__RequestVerificationToken: " + formData.__RequestVerificationToken);
+
+    var x = new XMLHttpRequest();
+    x = $.post(window.location, formData.serialize())
+    .done(function () {
+        $("#editDrinkContainer").html(x.responseText);
+        $("#editableDrinkSubmitButton").after("<span id=\"temporaryText\">Changes Saved!</span>");
+        var tempSpan = $("#temporaryText");
+        tempSpan.fadeOut(4500, function () {
+            tempSpan.remove();
+        });
+        $("#editableDrinkSubmitButton").click(AjaxSave);
+    });
+}
+
+$(document).ready(function () {
    
     $(document).ajaxError(function (event, jqxhr, settings, thrownError) {
         console.log("ajaxerror. Event: ");
@@ -11,17 +32,5 @@
         console.log(thrownError);
     });
 
-    $("#editableDrinkSubmitButton").click(function (event) {
-        event.preventDefault();
-   
-        var formData = $('#editDrinkForm');
-        formData.__RequestVerificationToken =  $('#editDrinkForm input[name="__RequestVerificationToken"]').val(); //there's one in the logout header. Make sure we have the *right* antiforgery token.
-
-        var x = new XMLHttpRequest();
-        x = $.post(window.location, formData.serialize())
-        .done(function () {
-            $("#editDrinkContainer").replaceWith($.parseHTML(x.responseText));
-        })
-
-    })
+    $("#editableDrinkSubmitButton").click(AjaxSave);
 });
